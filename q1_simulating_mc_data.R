fae2a978b0137cb1c22f9b54e4f41535547b730e
Rian Botes <55097034+rianhendrik@users.noreply.github.com>
2021-12-01 00:27:32 +0200

    Update q1_simulating_mc_data.R

--- q1_simulating_mc_data.R
+++ q1_simulating_mc_data.R
@@ -24,15 +24,15 @@ d = data.frame(mvrnorm(n, mu, Sigma, 5, 5))
 #Creating multicollinear variables
 x1 = d[,1]
 x2 = d[,2]
-x3 = 0.5*x1 + 1.1*x2 + rnorm(n, 0, 1)
-x4 = x1 + 0.9*x2 - x3 + rnorm(n, 0, 1)
+x3 = 0.5*x1 + 1.1*x2 + rnorm(n, 0, 0.002)
+x4 = x1 + 0.9*x2 - x3 + rnorm(n, 0, 0.002)
 t = cbind(x1, x2, x3, x4)
 #confirming high correlations
 cor(t)
 
 #creating the dependent variables
-y = 104 - 20*x1 - 13*x2 +         #creating the dependent variable
-  3.1*x3 + 40*x4 +rnorm(n, 0, 12) 
+y = 104 - 20*x1 + 13*x2 +         #creating the dependent variable
+  3.1*x3 + 40*x4 +rnorm(n, 0, ) 
 d = data.frame(y, x1, x2, x3, x4)   #creating a dataset
 cor(d)                              #checking correlation of dataset
 
@@ -40,7 +40,7 @@ mod = lm(y ~ x1 + x2 + x3 + x4, d)  #fitting model
 summary(mod)                        #note the insignificant variable estimates
 car::vif(mod)                       #vif shows beyond a doubt that we are dealing wiht MC.
 
-write.csv(d, "mc_data.csv")
+write.csv(d, "mc_data2.csv")
 
 
 
